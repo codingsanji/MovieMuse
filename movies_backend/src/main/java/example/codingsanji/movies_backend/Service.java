5c26ab4e01953654063c3061b1ec446374cb064b
Deydeepya <ddpyawork@gmail.com>
2024-11-05 20:36:56 +0300

    display singluar movies successfully

--- movies_backend/src/main/java/example/codingsanji/movies_backend/Service.java
+++ movies_backend/src/main/java/example/codingsanji/movies_backend/Service.java
@@ -1,16 +1,10 @@
 package example.codingsanji.movies_backend;
 
-<<<<<<< HEAD
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
->>>>>>> 7ed6d6ff396020838580b9e3941feed541df43bd
 
 @org.springframework.stereotype.Service
 /*Database Access Methods*/
@@ -20,11 +14,8 @@ public class Service {
     public List<Movie> allMovies(){
         return movieRepository.findAll();
     }
-<<<<<<< HEAD
 
     public Optional<Movie> singleMovie(ObjectId id){ /*Optional since if there is no such id itll import null*/
         return movieRepository.findById(id);
     }
-=======
->>>>>>> 7ed6d6ff396020838580b9e3941feed541df43bd
 }
